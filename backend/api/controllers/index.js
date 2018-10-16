b7f997d5b66e109f40f0f1de5f6c5b176aa87696
Cliff Kang <inlovewithhim@gmail.com>
2018-10-15 22:47:21 -0700

    basic routes setup

--- backend/api/controllers/index.js
+++ backend/api/controllers/index.js
@@ -1,8 +1,10 @@
 const { createUser } = require('./user');
 const { createChannel, getAllChannels } = require('./channel');
+const { createVideo } = require('./video');
 
 module.exports = {
   createUser,
   createChannel,
   getAllChannels,
-}
+  createVideo,
+};
--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,5 +1,13 @@
-const { } = require('../controllers');
+const { 
+  createUser,
+  createChannel,
+  getAllChannels,
+  createVideo,
+} = require('../controllers');
 
 module.exports = server => {
-    server.route('').verb();
-}
+  server.route('/user').post(createUser);
+  server.route('/channel').post(createChannel);
+  server.route('/channels').get(getAllChannels);
+  server.route('/video').post(createVideo);
+};
